c1b7971dbcfcb26ad43d00d9a13479c7cc6fb526
Francisco Massano <francis.massano@gmail.com>
2021-11-12 21:20:08 +0000

    Add files via upload

---
+++ Bootcamps/Unity3D GameArena/Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public int pontos1;
+    public int pontos2;
+
+    public Text score1;
+    public Text score2;
+
+    private void Start()
+    {
+        pontos1 = 0;
+        pontos2 = 0;
+    }
+
+    public IEnumerator restart()
+    {
+        score1.text = "Player1: " + pontos1;
+        score2.text = "Player2: " + pontos2;
+
+        yield return new WaitForSeconds(1.9f);
+        player1.transform.position = new Vector3(-7, 3, 0);
+        player1.GetComponent<MeshRenderer>().enabled = true;
+        player1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        player1.GetComponent<Player1Movement>().isDying = false;
+
+        player2.transform.position = new Vector3(7, 3, 0);
+        player2.GetComponent<MeshRenderer>().enabled = true;
+        player2.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        player2.GetComponent<Player2Movement>().isDying = false;
+
+
+    }
+}
---
+++ Bootcamps/Unity3D GameArena/Assets/Player1Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    public Renderer rend;
+    public Rigidbody body;
+    public float speed;
+    public ParticleSystem explosion;
+    public GameManager manager;
+    public bool isDying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDying = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(transform.position.y < -20 && isDying == false)
+        {
+            isDying = true;
+            rend.enabled = false;
+            body.constraints = RigidbodyConstraints.FreezeAll;
+            explosion.Play();
+            manager.pontos2 = manager.pontos2 + 1;
+            manager.StartCoroutine("restart");
+        }
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(Vector3.back * speed * Time.deltaTime);
+        }
+    }
+
+
+
+}
