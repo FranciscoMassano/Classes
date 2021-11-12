a70ee1968fc85af68c3d58feae6f50dffcebd0b0
Francisco Massano <francis.massano@gmail.com>
2021-11-12 21:22:06 +0000

    Add files via upload

---
+++ Bootcamps/Unity3D GameRun/Assets/GroundChange1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChange1 : MonoBehaviour
+{
+    bool isWorking;
+
+    public int fase;
+    public Renderer rend;
+    public Material verde;
+    public Material amarelo;
+    public Material vermelho;
+    public GameObject brokenGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fase = 0;
+        isWorking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(isWorking == false)
+            {
+                StartCoroutine(Change());
+            }
+        }
+    }
+
+    IEnumerator Change()
+    {
+         isWorking = true;
+         fase = fase + 1;
+         if (fase == 1)
+         {
+            rend.material = verde;
+            yield return new WaitForSeconds(1);
+         }
+
+         if (fase == 2)
+         {
+            rend.material = amarelo;
+            yield return new WaitForSeconds(1);
+         }
+
+         if (fase == 3)
+         {
+            rend.material = vermelho;
+            yield return new WaitForSeconds(2);
+            Instantiate(brokenGround, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+         }
+
+        isWorking = false;
+    }
+}
---
+++ Bootcamps/Unity3D GameRun/Assets/Morte.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morte : MonoBehaviour
+{
+    public GameManager manager;
+    public string cor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -20)
+        {
+            manager.Death(cor);
+        }
+    }
+}
